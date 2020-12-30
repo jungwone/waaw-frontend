ac8caa2d7336da7e4d57ca6e28f52174950182f0
spcjw <prsjb77@gmail.com>
2020-12-30 12:29:51 +0900

    fix banner style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -19,6 +19,7 @@ const Wrapper = styled.div`
   width: 100%;
   height: 400px;
   position: relative;
+  top: 70px;
   background-image: url(${(props) => props.src});
   background-repeat: no-repeat;
   background-position: center;
